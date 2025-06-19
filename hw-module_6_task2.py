ab95e81c8a17db0876f8b71dedb16b45e0cbc381
InnaKeba <kebainna@gmail.com>
2025-06-19 20:43:43 +0300

    Added get_cats_info(path) function to parse cat data from file and return structured list.

--- hw-module_6_task2.py
+++ hw-module_6_task2.py
@@ -16,3 +16,22 @@ cats_info = [
 with open ("cats_data.txt", "w", encoding="utf-8") as file:  # Створення файлу з даними про котиків
     for entry in cats_info:
         file.write(entry + "\n")
+def get_cats_info(path):
+    try:
+        with open(path, encoding="utf-8") as file:
+            text_from_cats_data = file.read()  # Читання вмісту файлу txt
+        entries = text_from_cats_data.strip().split("\n")  # Розбиваємо текст
+        cats_list = []              
+        for entry in entries:
+            try:
+                cat_id, name, age = entry.split(",")  # Розділення по комі на id котиків, імені та віку 
+                cats_list.append({"id": cat_id, "name": name, "age": int(age)})  # Створення словника з інформацією про котика
+            except ValueError:  
+                continue 
+        return cats_list
+    except FileNotFoundError:
+        print("Файл не знайдено. Перевірте шлях до файлу.")
+        return []
+    except Exception as e:
+        print(f"Сталася помилка при читанні файлу: {e}")
+        return []
